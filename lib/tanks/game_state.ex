da5b72c15c3ba7d69ceb164755f99d759ee660bf
Szabo Gergely <gszabo@yahoo-corp.jp>
2019-01-25 21:25:31 +0900

    firing a bullet

--- lib/tanks/game_state.ex
+++ lib/tanks/game_state.ex
@@ -6,14 +6,19 @@ defmodule Field do
     newX = (object_x + velocity_x) |> min(@field_width - object_width) |> max(0)
     newY = (object_y + velocity_y) |> min(@field_height - object_height) |> max(0)
 
-    {newX, newY}
+    cond do
+      newX < 0 or newX > @field_width - object_width -> :error
+      newY < 0 or newY > @field_height - object_height -> :error
+      true -> {:ok, newX, newY}
+    end
   end
 end
 
 defmodule GameState do
   use GenServer
 
-  defstruct tanks: Map.new()
+  defstruct tanks: Map.new(),
+            bullets: []
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts ++ [name: __MODULE__])
@@ -25,6 +30,14 @@ defmodule GameState do
 
   def get_tanks do
     GenServer.call(__MODULE__, :get_tanks)
+  end
+
+  def get_bullets do
+    GenServer.call(__MODULE__, :get_bullets)
+  end
+
+  def fire(tankId) do
+    GenServer.cast(__MODULE__, {:fire, tankId})
   end
 
   def remove_tank(tankId) do
@@ -45,18 +58,19 @@ defmodule GameState do
   # Evaluate movement
   def handle_info(:tick, state) do
     state.tanks |> Map.values() |> Enum.map(&Tank.move(&1))
+    bullets = state.bullets |> Enum.map(&Bullet.move(&1))
     schedule_tick()
-    {:noreply, state}
+    {:noreply, %GameState{state | bullets: bullets}}
   end
 
   # Create a new tank
   def handle_call({:create_tank, tankId}, _from, state) do
     if !Map.has_key?(state.tanks, tankId) do
       {:ok, tankPid} = Tank.start_link([])
-      newState = %{state | tanks: state.tanks |> Map.put_new(tankId, tankPid)}
+      newState = %GameState{state | tanks: state.tanks |> Map.put_new(tankId, tankPid)}
       {:reply, {:ok, tankPid}, newState}
     else
-      {:reply, {:err, "Already existing"}, state}
+      {:reply, {:error, "Already existing"}, state}
     end
   end
 
@@ -70,9 +84,25 @@ defmodule GameState do
     {:reply, tanks, state}
   end
 
+  # Get all bullets
+  def handle_call(:get_bullets, _from, state) do
+    {:reply, state.bullets, state}
+  end
+
+  def handle_cast({:fire, tankId}, state) do
+    case Map.fetch(state.tanks, tankId) do
+      {:ok, tankPid} ->
+        newState = %GameState{state | bullets: [Tank.fire(tankPid) | state.bullets]}
+        {:noreply, newState}
+
+      :error ->
+        {:noreply, state}
+    end
+  end
+
   # Remove tank
   def handle_cast({:remove_tank, tankId}, state) do
-    newState = %{state | tanks: Map.delete(state.tanks, tankId)}
+    newState = %GameState{state | tanks: Map.delete(state.tanks, tankId)}
     {:noreply, newState}
   end
 end
--- lib/tanks/tank.ex
+++ lib/tanks/tank.ex
@@ -1,9 +1,30 @@
+defmodule Bullet do
+  @enforce_keys [:x, :y, :velocity_x, :velocity_y]
+  defstruct [:x, :y, :velocity_x, :velocity_y]
+
+  def move(bullet) do
+    movement = Field.move_object(3, bullet.x, bullet.velocity_x, 3, bullet.y, bullet.velocity_y)
+
+    case movement do
+      {:ok, newX, newY} -> %Bullet{bullet | x: newX, y: newY}
+      :error -> :error
+    end
+  end
+end
+
 defmodule Tank do
   use GenServer
 
   @max_velocity 5
 
-  defstruct health: 100, width: 50, height: 20, x: 0, y: 0, velocity: 0
+  defstruct health: 100,
+            width: 50,
+            height: 20,
+            x: 0,
+            y: 0,
+            velocity: 0,
+            direction: :right,
+            turretAngle: 0
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
@@ -14,6 +35,7 @@ defmodule Tank do
   end
 
   # Validate and set velocity
+  @spec set_velocity(atom() | pid() | {atom(), any()} | {:via, atom(), any()}, any()) :: :ok
   def set_velocity(tankPid, velocity) do
     GenServer.cast(tankPid, {:set_velocity, velocity})
   end
@@ -28,6 +50,10 @@ defmodule Tank do
     GenServer.call(tankPid, :get_state)
   end
 
+  def fire(tankPid) do
+    GenServer.call(tankPid, :fire)
+  end
+
   def handle_cast({:set_velocity, velocity}, tank) do
     newTank = %Tank{tank | velocity: velocity |> min(@max_velocity) |> max(-@max_velocity)}
 
@@ -35,13 +61,38 @@ defmodule Tank do
   end
 
   def handle_cast(:move, tank) do
-    {newX, newY} = Field.move_object(tank.width, tank.x, tank.velocity, tank.height, tank.y)
-    newTank = %Tank{tank | x: newX, y: newY}
+    movement = Field.move_object(tank.width, tank.x, tank.velocity, tank.height, tank.y)
 
-    {:noreply, newTank}
+    case movement do
+      {:ok, newX, newY} -> {:noreply, %Tank{tank | x: newX, y: newY}}
+      :error -> {:noreply, tank}
+    end
   end
 
   def handle_call(:get_state, _from, tank) do
     {:reply, tank, tank}
+  end
+
+  def handle_call(:fire, _from, tank) do
+    bullet =
+      case tank.direction do
+        :left ->
+          %Bullet{
+            x: tank.x + 20 - round(20 * :math.cos(tank.turretAngle)),
+            y: tank.y + 14 - round(20 * :math.sin(tank.turretAngle)),
+            velocity_x: round(-800 * :math.cos(tank.turretAngle)),
+            velocity_y: round(-800 * :math.sin(tank.turretAngle))
+          }
+
+        :right ->
+          %Bullet{
+            x: tank.x + 50 + round(20 * :math.cos(tank.turretAngle)),
+            y: tank.y + 14 - round(20 * :math.sin(tank.turretAngle)),
+            velocity_x: round(800 * :math.cos(tank.turretAngle)),
+            velocity_y: round(-800 * :math.sin(tank.turretAngle))
+          }
+      end
+
+    {:reply, bullet, tank}
   end
 end
