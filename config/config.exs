fb10ddfd5cf0fc3f9c2fb11482098403b6ba5d0f
Gergo <gege251@gmail.com>
2019-02-07 12:20:22 +0900

    Upgrade dependencies

--- config/config.exs
+++ config/config.exs
@@ -20,8 +20,8 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Config property-based tesing wiht ExCheck
-config :excheck, :number_iterations, 200
+# # Config property-based tesing wiht ExCheck
+# config :excheck, :number_iterations, 200
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
