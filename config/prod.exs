3b81e518168c07991f1838973122e56a1e7b194a
Gergo <gege251@gmail.com>
2019-02-11 10:47:56 +0900

    Deployment config

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,9 @@ use Mix.Config
 config :tanks, TanksWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -66,4 +68,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
