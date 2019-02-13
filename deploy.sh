7be25a2e3ab212a55975c0c6c0794634d1b1e5d9
Szabo Gergely <gszabo@yahoo-corp.jp>
2019-02-13 10:58:54 +0900

    Fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
  git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
- mix test && git push -f gigalixir HEAD:refs/heads/master
+ git push -f gigalixir HEAD:refs/heads/master
