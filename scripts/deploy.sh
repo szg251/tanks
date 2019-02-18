e6864f1da46a5f72fb40e7d56dc279f2c810c435
Gergo <gege251@gmail.com>
2019-02-18 09:34:21 +0900

    Echo logs before deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
- git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
- git push -f gigalixir HEAD:refs/heads/master
+printf "\nStarting deployment to Gigalixir\n\n"
+
+git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
+git push -f gigalixir HEAD:refs/heads/master
